790e17db58fade280707c7c0cea5ae37d44e964e
HuesosAce <pauloarielyepez@gmail.com>
2023-06-21 23:29:54 -0500

    feat: add Prueba huesos

--- src/main/java/com/gym/pruebas/Pruebas.java
+++ src/main/java/com/gym/pruebas/Pruebas.java
@@ -7,5 +7,6 @@ import com.gym.factory.ConnectionFactory;
 public class Pruebas {
 	public static void main(String[] args) {
 		Connection con = new ConnectionFactory().conectar();
+		System.out.println("xd");
 	}
 }
